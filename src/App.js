e7099493d67aad48c134aeaa0930727c68d8e070
Kanchan Chaudhary <kanchan.chaudhary77@gmail.com>
2021-11-16 18:53:49 +0530

    created table of countries with number of cases

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import { FormControl, MenuItem, Select } from '@mui/material';
+import { Card, CardContent, FormControl, MenuItem, Select } from '@mui/material';
 import { useEffect, useState } from 'react';
 import './app.css';
+import InfoBox from './components/InfoBox';
+import Map from './components/Map';
+import Table from './components/Table';
+import { sortData } from './utlitity';
+import Graph from './components/Graph';
 
 // https://disease.sh/v3/covid-19/countries
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('Worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
+
+  // for fetching worldwide data 
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/all')
+      .then(response => response.json())
+      .then(data => {
+        setCountryInfo(data);
+      });
+  }, [])
+
+  // for api call to fetch country name, value and id to loop in menu-items 
   useEffect(() => {
     const getCountriesData = async () => {
       await fetch('https://disease.sh/v3/covid-19/countries')
@@ -19,48 +37,70 @@ function App() {
               value: country.countryInfo.iso2,
               id: country.countryInfo.id
             }));
+
+          const sortedData = sortData(data)
           setCountries(countries);
+          setTableData(sortedData);
         });
     }
     getCountriesData();
 
   }, [])
 
+  // for fetching the data for automatic case numbers country wise and worldwide
   const onCountryChange = async (event) => {
     const countryCode = event.target.value;
-    setCountry(countryCode);
-  }
+    const url =
+      countryCode === 'worldwide'
+        ? 'https://disease.sh/v3/covid-19'
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      });
+  };
+
+  // console.log(countryInfo)
 
   return (
     <div className="app">
-      <div className="app_header">
-        <h1 className="app_title">Covid-19 Tracker</h1>
-        <FormControl className="app_dropdown">
-          <Select
-            variant="outlined"
-            value="something"
-            key={countries.id}
-            value={country}
-            onChange={onCountryChange}
-          >
-            <MenuItem value="Worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+      <div className="app_left">
+        <div className="app_header">
+          <h1 className="app_title">Covid-19 Tracker</h1>
+          <FormControl className="app_dropdown">
+            <Select
+              variant="outlined"
+              value="something"
+              key={countries.id}
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="Worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="app_stats">
+          <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+        </div>
+        <Map />
       </div>
 
-      <div className="app_stats">
-        {/* infobox */}
-        {/* infobox */}
-        {/* infobox */}
-      </div>
-
-      {/* table  */}
-      {/* graph  */}
-
-      {/* map  */}
+      <Card className="app_right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData} />
+          <h3>Worldwide New Cases</h3>
+          <Graph />
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -1,7 +1,22 @@
+import { Card, CardContent, Typography } from "@mui/material";
 import React from "react";
 
-const InfoBox = () => {
-  return <div></div>;
+const InfoBox = ({ title, cases, total }) => {
+  return (
+    <div>
+      <Card className="infoBox">
+        <CardContent>
+          <Typography className="infoBox_title" color="textSecondary">
+            {title}
+          </Typography>
+          <h3 className="infoBox_cases">{cases ? cases + " Today" : ""}</h3>
+          <Typography className="infoBox_total" color="textSecondary">
+            {total} Total
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default InfoBox;
