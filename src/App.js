a0322bb3416ccdf50b4fe060d7229eb40ea14aa7
Kanchan Chaudhary <kanchan.chaudhary77@gmail.com>
2021-11-16 17:24:58 +0530

    header section completed with dropdown

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('Worldwide');
 
-
   useEffect(() => {
     const getCountriesData = async () => {
       await fetch('https://disease.sh/v3/covid-19/countries')
@@ -27,27 +26,36 @@ function App() {
 
   }, [])
 
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  }
+
   return (
     <div className="app">
       <div className="app_header">
         <h1 className="app_title">Covid-19 Tracker</h1>
         <FormControl className="app_dropdown">
-          <Select variant="outlined" value="something" key={countries.id} value={country}>
+          <Select
+            variant="outlined"
+            value="something"
+            key={countries.id}
+            value={country}
+            onChange={onCountryChange}
+          >
             <MenuItem value="Worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
-
           </Select>
         </FormControl>
       </div>
-      {/* header  */}
-      {/* title and dropdowns  */}
 
-
-      {/* infobox */}
-      {/* infobox */}
-      {/* infobox */}
+      <div className="app_stats">
+        {/* infobox */}
+        {/* infobox */}
+        {/* infobox */}
+      </div>
 
       {/* table  */}
       {/* graph  */}
