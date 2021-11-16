3540c5d09e1d16a5a6b9a0dd9c8644122d2afb1b
Kanchan Chaudhary <kanchan.chaudhary77@gmail.com>
2021-11-16 16:31:48 +0530

    created the required components and installed MUI

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './app.css';
 function App() {
   return (
     <div className="app">
+      <h1>Covid Tracker App</h1>
+      {/* header  */}
+      {/* title and dropdowns  */}
 
+
+      {/* infobox */}
+      {/* infobox */}
+      {/* infobox */}
+
+      {/* table  */}
+      {/* graph  */}
+
+      {/* map  */}
     </div>
   );
 }
