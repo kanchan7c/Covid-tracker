70d49c7a64cec83839ef014d4a902c1518b557d0
Kanchan Chaudhary <kanchan.chaudhary77@gmail.com>
2021-11-16 17:15:39 +0530

    API call made for list of countries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import { FormControl, MenuItem, Select } from '@mui/material';
+import { useEffect, useState } from 'react';
 import './app.css';
 
+// https://disease.sh/v3/covid-19/countries
+
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('Worldwide');
+
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso2,
+              id: country.countryInfo.id
+            }));
+          setCountries(countries);
+        });
+    }
+    getCountriesData();
+
+  }, [])
+
   return (
     <div className="app">
-      <h1>Covid Tracker App</h1>
+      <div className="app_header">
+        <h1 className="app_title">Covid-19 Tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="something" key={countries.id} value={country}>
+            <MenuItem value="Worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+
+          </Select>
+        </FormControl>
+      </div>
       {/* header  */}
       {/* title and dropdowns  */}
 
